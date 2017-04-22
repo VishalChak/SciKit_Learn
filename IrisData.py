63e30d4335f12d4d8ada8de20dfb43caf22ebe86
VishalChak <vishalchak.nitk@gmail.com>
2017-04-22 11:52:14 +0530

    10 Essential_ML_Algo

--- IrisData.py
+++ IrisData.py
@@ -2,17 +2,9 @@ import numpy as np
 from sklearn.datasets import load_iris
 
 class IrisData:
-	def printData(self):
+	def load_iris_data():
 		iris = load_iris()
 		X = iris.data
 		y = iris.target
 		z = iris.target_names
-		f = iris.feature_names
-		print(X)
-		#print(y)
-		#print(f)
-					
-
-if __name__ == "__main__":
-	obj = IrisData()
-	obj.printData()
+		return X,y,z
